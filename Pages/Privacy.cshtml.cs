0ed9c0b74fafb41c2cb3460dac93ea47af970576
Santiago Mayaud <santiago.mayaud@istea.com.ar>
2023-04-03 11:14:38 -0300

    Initial Commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace clase2.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    [BindProperty] //Lo que llega de un formulario, si tiene las mismas propiedades, se ingresa
+    public Form Data {get; set;}
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        var parametro = Request.Query["parametrox"];
+        Data = new Form();
+        Data.Mail = parametro;
+        Data.Password = "1234";
+    }
+
+    public IActionResult OnPost(){ //Este código revisa que el pedido de información siga el modelo
+        if(!ModelState.IsValid){ //Este código significa "Si el modelo no es correcto"
+            return Page();
+        }
+        // else, guardar en base de datos, procesar información, etc
+        var formValues = Data;
+        
+        return RedirectToAction("Get");
+    }
+}
+
